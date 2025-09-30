bc0ab5509554ff075e8c6fc10fbd7a462e75be54
Luis Perez <luis.perez.3@asu.edu>
2025-09-30 19:50:09 +0000

    Update prompt image styles for improved responsiveness and enhance word images in vocabulary list

--- js/words.js
+++ js/words.js
@@ -5,37 +5,37 @@ const WORDS = [
   { en: "dog",     es: "perro",      cat: "Animals",   img: "https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg" },
   { en: "cat",     es: "gato",       cat: "Animals",   img: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg" },
   { en: "bird",    es: "pájaro",     cat: "Animals",   img: "https://upload.wikimedia.org/wikipedia/commons/b/b3/Female_house_sparrow_at_Kodai.jpg" },
-  { en: "fish",    es: "pez",        cat: "Animals",   img: "https://upload.wikimedia.org/wikipedia/commons/1/12/Fish_01.jpg" },
+  { en: "fish",    es: "pez",        cat: "Animals",   img: "https://upload.wikimedia.org/wikipedia/commons/a/ad/Amphiprion_ocellaris_%28Clown_anemonefish%29_by_Nick_Hobgood.jpg" },
 
   // Food
   { en: "apple",   es: "manzana",    cat: "Food",      img: "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg" },
-  { en: "bread",   es: "pan",        cat: "Food",      img: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Bread_1.jpg" },
-  { en: "milk",    es: "leche",      cat: "Food",      img: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Milk_glass.jpg" },
-  { en: "cheese",  es: "queso",      cat: "Food",      img: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Cheese.jpg" },
+  { en: "bread",   es: "pan",        cat: "Food",      img: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Breads_of_Moskovskaya_Oblast._img_006.jpg" },
+  { en: "milk",    es: "leche",      cat: "Food",      img: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Milk_glass.jpg" },
+  { en: "cheese",  es: "queso",      cat: "Food",      img: "https://upload.wikimedia.org/wikipedia/commons/0/04/Kraft_Singles.jpg" },
 
   // Household
-  { en: "chair",   es: "silla",      cat: "Household", img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Chair.jpg" },
-  { en: "table",   es: "mesa",       cat: "Household", img: "https://upload.wikimedia.org/wikipedia/commons/6/6b/Table.jpg" },
-  { en: "window",  es: "ventana",    cat: "Household", img: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Window.jpg" },
-  { en: "door",    es: "puerta",     cat: "Household", img: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Door.jpg" },
+  { en: "chair",   es: "silla",      cat: "Household", img: "https://upload.wikimedia.org/wikipedia/commons/8/83/Side_Chair_MET_213686.jpg" },
+  { en: "table",   es: "mesa",       cat: "Household", img: "https://upload.wikimedia.org/wikipedia/commons/3/31/Dinner_table_side_pine_wood_large_table_2.png" },
+  { en: "window",  es: "ventana",    cat: "Household", img: "https://upload.wikimedia.org/wikipedia/commons/6/64/Open-air_Museum_Vyso%C4%8Dina_%28by_Pudelek%29_02.jpg" },
+  { en: "door",    es: "puerta",     cat: "Household", img: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Yellow_Door%2C_Red_Door._-_geograph.org.uk_-_526824.jpg" },
 
   // School
-  { en: "book",    es: "libro",      cat: "School",    img: "https://upload.wikimedia.org/wikipedia/commons/1/1d/Book.jpg" },
-  { en: "pencil",  es: "lápiz",      cat: "School",    img: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Pencil.jpg" },
-  { en: "teacher", es: "maestro",    cat: "School",    img: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Teacher.jpg" },
-  { en: "school",  es: "escuela",    cat: "School",    img: "https://upload.wikimedia.org/wikipedia/commons/3/3c/School.jpg" },
+  { en: "book",    es: "libro",      cat: "School",    img: "https://upload.wikimedia.org/wikipedia/commons/9/9c/Books_for_free_2023_20230302_095511.jpg" },
+  { en: "pencil",  es: "lápiz",      cat: "School",    img: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Yellow-pencil.svg" },
+  { en: "teacher", es: "maestro",    cat: "School",    img: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Teaching_icon.png" },
+  { en: "school",  es: "escuela",    cat: "School",    img: "https://upload.wikimedia.org/wikipedia/commons/d/d4/A_Hayesville_High_School_classroom_in_Clay_County%2C_N.C.%2C_in_2004.jpg" },
 
   // Nature
-  { en: "sun",     es: "sol",        cat: "Nature",    img: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Sun.jpg" },
-  { en: "moon",    es: "luna",       cat: "Nature",    img: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Moon.jpg" },
-  { en: "water",   es: "agua",       cat: "Nature",    img: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Water.jpg" },
-  { en: "tree",    es: "árbol",      cat: "Nature",    img: "https://upload.wikimedia.org/wikipedia/commons/2/2b/Tree.jpg" },
+  { en: "sun",     es: "sol",        cat: "Nature",    img: "https://upload.wikimedia.org/wikipedia/commons/0/0b/Sun_in_the_sands.jpg" },
+  { en: "moon",    es: "luna",       cat: "Nature",    img: "https://upload.wikimedia.org/wikipedia/commons/d/dd/Full_Moon_Luc_Viatour.jpg" },
+  { en: "water",   es: "agua",       cat: "Nature",    img: "https://upload.wikimedia.org/wikipedia/commons/1/11/Drops_Impact.jpg" },
+  { en: "tree",    es: "árbol",      cat: "Nature",    img: "https://upload.wikimedia.org/wikipedia/commons/d/db/Leaning_trees_over_the_grassy_hillock_of_Nodong-ri_tombs_clouds_and_blue_sky_in_Gyeongju_South_Korea.jpg" },
 
   // Travel / Places
-  { en: "car",     es: "coche",      cat: "Travel",    img: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Car.jpg" },
-  { en: "house",   es: "casa",       cat: "Travel",    img: "https://upload.wikimedia.org/wikipedia/commons/5/5c/House.jpg" },
-  { en: "street",  es: "calle",      cat: "Travel",    img: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Street.jpg" },
-  { en: "city",    es: "ciudad",     cat: "Travel",    img: "https://upload.wikimedia.org/wikipedia/commons/1/1a/City.jpg" },
+  { en: "car",     es: "coche",      cat: "Travel",    img: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Nissan_Skyline_GTR_R34%2C_Bangladesh._%2828331206878%29.jpg" },
+  { en: "house",   es: "casa",       cat: "Travel",    img: "https://upload.wikimedia.org/wikipedia/commons/c/c9/Ranch_style_home_in_Salinas%2C_California.JPG" },
+  { en: "street",  es: "calle",      cat: "Travel",    img: "https://upload.wikimedia.org/wikipedia/commons/5/51/Wilfrid_Road_-_High_Street_-_geograph.org.uk_-_4989624.jpg" },
+  { en: "city",    es: "ciudad",     cat: "Travel",    img: "https://upload.wikimedia.org/wikipedia/commons/d/de/St_Louis_night_expblend.jpg" },
 ];
 
 // Derive the list of categories from the words.
